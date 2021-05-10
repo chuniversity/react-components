557550434853d429f6777ec402a8dc3cfed3c2ce
Chuniversity <contact@chunis.org>
2021-05-10 13:22:13 -0400

    complete

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// TODO
+
+
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bold: false
+    }
+  }
+  onGroceryListItemHover() {
+    this.setState({
+      bold: !this.state.bold
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bolder' : 'normal'
+    };
+
+    return (
+      <ul>
+        <li style={style} onMouseOver={this.onGroceryListItemHover.bind(this)}>{this.props.item}</li>
+      </ul>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+
+
+var App = () => (
+  <GroceryList items={['Cucumber', 'Kale']} />
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
